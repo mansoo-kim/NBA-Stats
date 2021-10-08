d04f2c158982fc9c146389ac912a5b0c3060d300
Mansoo Kim <mansookim09@gmail.com>
2021-10-07 23:21:59 -0400

    Add importing csv file

--- src/plot.js
+++ src/plot.js
@@ -1,7 +1,20 @@
 import * as d3 from "d3";
 
+const DEFAULT_Y = "PTS"
+const DEFAULT_X = "MP"
+
 export default class Plot {
-  contructor(dimensions) {
+
+  constructor(dimensions) {
     [this.x, this.y] = dimensions;
+    this.svg = d3.select(".scatter").append("svg")
+    this.getYear(2021);
+  }
+
+  getYear(year) {
+    d3.text(`src/data/${year-1}-${year}-per-game.csv`).then(text => {
+      this.data2021 = d3.csvParse(text);
+      console.log(this.data2021)
+    })
   }
 }
