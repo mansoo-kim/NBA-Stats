ebea3b60cd511af0a8c0a8ee7f916a88f050e4c2
Mansoo Kim <mansookim09@gmail.com>
2021-10-09 18:58:51 -0400

    Add python script to combine stats/csvs

---
+++ src/data/combine-stats.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import json
+
+years = [2021]
+
+with open('all-stars.json') as json_data:
+    all_stars_dict = json.load(json_data)
+
+for year in years:
+    # Read in two csvs
+    per_game = pd.read_csv("{}-{}-per-game.csv".format(year-1, year))
+    advanced = pd.read_csv("{}-{}-advanced.csv".format(year-1, year))
+
+    # Drop duplicate/unecessary columns in advanced csv
+    advanced.drop(['Rk', 'Pos', 'Age', 'Tm', 'MP', 'Unnamed: 19', 'Unnamed: 24'], axis=1, inplace=True)
+
+    # Merge/Join two csvs
+    merged = per_game.merge(advanced, on="Player")
+
+    # Format Player name properly
+    merged['Player'] = merged['Player'].map(lambda x: x.split('\\')[0])
+
+    # Set All-Star status
+    merged["All-Star"] = "false"
+    merged.loc[merged['Player'].isin(all_stars_dict[str(year)]), "All-Star"] = "true"
+
+    # Save the combined stats to csv
+    merged.to_csv("{}-{}-stats.csv".format(year-1, year), index=False)
