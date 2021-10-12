a9f9c6a8e77aa09d9b6237ca47606840f851fb99
Mansoo Kim <mansookim09@gmail.com>
2021-10-12 16:33:20 -0400

    Add ml5 library

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import Plot from "./scripts/plot";
 import setup from "./scripts/setup";
+import Plot from "./scripts/plot";
+import train from "./scripts/train";
 
 setup();
 const plot = new Plot();
+train();
---
+++ src/scripts/train.js
@@ -0,0 +1,3 @@
+export default () => {
+  console.log('ml5 version:', ml5.version);
+}
